7f98baf0d98596d8e49ad83e9a3a0c3cdc9e6b70
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-23 17:55:19 -0300

    Adicionar arquivos de projeto.

---
+++ matrizesMostrarDiagonalENegativos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Digite a quantidade numeros da matriz, em seguida digite os valores da matrizes:  ");
+
+
+            int N;
+            int[,] A;
+
+            N = int.Parse(Console.ReadLine());
+            A = new int[N, N];
+            
+            for (int i=0; i<N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j=0; j<N; j++)
+                {
+                    A[i,j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Diaonal principal: ");
+            for(int i=0; i<N; i++)
+            {
+                Console.Write(A[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int cont = 0;
+            for (int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    if (A[i,j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+            Console.WriteLine("Quantidade de negativos= " + cont);
+
+
+        }
+    }
+}
